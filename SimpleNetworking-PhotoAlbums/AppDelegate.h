df4b6b0ff95b16bf2119c177465a7ec39058c8d9
Dylan Sewell <dvsewell15@gmail.com>
2019-08-13 12:29:28 -0700

    Initial Commit

---
+++ SimpleNetworking-PhotoAlbums/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SimpleNetworking-PhotoAlbums
+//
+//  Created by Dylan Sewell on 8/13/19.
+//  Copyright © 2019 dylansewell. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SimpleNetworking-PhotoAlbumsUITests/SimpleNetworking_PhotoAlbumsUITests.m
@@ -0,0 +1,38 @@
+//
+//  SimpleNetworking_PhotoAlbumsUITests.m
+//  SimpleNetworking-PhotoAlbumsUITests
+//
+//  Created by Dylan Sewell on 8/13/19.
+//  Copyright © 2019 dylansewell. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface SimpleNetworking_PhotoAlbumsUITests : XCTestCase
+
+@end
+
+@implementation SimpleNetworking_PhotoAlbumsUITests
+
+- (void)setUp {
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+
+    // In UI tests it is usually best to stop immediately when a failure occurs.
+    self.continueAfterFailure = NO;
+
+    // UI tests must launch the application that they test. Doing this in setup will make sure it happens for each test method.
+    [[[XCUIApplication alloc] init] launch];
+
+    // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+}
+
+- (void)testExample {
+    // Use recording to get started writing UI tests.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+@end
